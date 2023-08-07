802b50e8c3b859750f150cd3d824702df9421f63
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-08-08 06:48:54 +0800

    feat: add get /todo/id handler

--- src/handler.rs
+++ src/handler.rs
@@ -4,8 +4,8 @@ use crate::{
 };
 
 use actix_web::{delete, get, patch, post, web, HttpResponse, Responder};
-// use chrono::prelude::*;
-// use uuid::Uuid;
+use chrono::prelude::*;
+use uuid::Uuid;
 
 #[get("/healthchecker")]
 async fn health_checker_handler() -> impl Responder {
@@ -39,12 +39,76 @@ pub async fn todos_list_handler (
     HttpResponse::Ok().json(json_response)
 }
 
+#[post("/todos")]
+async fn create_todo_handler(
+    mut body: web::Json<Todo>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let mut vec = data.todo_db.lock().unwrap();
 
+    let todo = vec.iter().find(|todo| todo.title == body.title);
+
+    if todo.is_some(){
+        let error_response = GenericResponse{
+            status: "fail".to_string(),
+            message: format!("Todo with title: '{}' already exists", body.title),
+        };
+        return HttpResponse::Conflict().json(error_response);
+    }
+
+    let uuid_id = Uuid::new_v4();
+    let datetime = Utc::now();
+    
+    body.id = Some(uuid_id.to_string());
+    body.completed = Some(false);
+    body.createdAt = Some(datetime);
+    body.updatedAt = Some(datetime);
+
+    let todo = body.to_owned();
+
+    vec.push(body.into_inner());
+
+    let json_response = SingleTodoResponse {
+        status: "success".to_string(),
+        data: TodoData { todo },
+    };
+
+    HttpResponse::Ok().json(json_response)
+
+}
+
+#[get("/todo/{id}")]
+async fn get_todo_handler(
+    path: web::Path<String>, 
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let vec = data.todo_db.lock().unwrap();
+
+    let id = path.into_inner();
+    let todo = vec.iter().find(|todo| todo.id == Some(id.to_owned()));
+
+    if todo.is_none() {
+        let error_response = GenericResponse {
+            status: "fail".to_string(),
+            message: format!("Todo with ID: {} not found", id),
+        };
+        return HttpResponse::NotFound().json(error_response);
+    }
+
+    let todo = todo.unwrap();
+    let json_response = SingleTodoResponse {
+        status: "success".to_string(),
+        data: TodoData { todo: todo.clone() },
+    };
+    HttpResponse::Ok().json(json_response)
+}
 
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/api")
         .service(health_checker_handler)
-        .service(todos_list_handler);
+        .service(todos_list_handler)
+        .service(create_todo_handler)
+        .service(get_todo_handler);
 
     conf.service(scope);
 }
