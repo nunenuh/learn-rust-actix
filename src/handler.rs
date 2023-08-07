f3217517d654e57a78e29488792f5729d8ce5ef0
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-08-08 00:05:35 +0800

    feat: add model, response and handler for get todos

---
+++ src/handler.rs
@@ -0,0 +1,41 @@
+use crate::{
+    model::{AppState, QueryOptions, Todo, UpdateTodoSchema},
+    response::{GenericResponse, SingleTodoResponse, TodoData, TodoListResponse},
+};
+
+use actix_web::{delete, get, patch, post, web, HttpResponse, Responder};
+use chrono::prelude::*;
+use uuid::Uuid;
+
+#[get("/healthchecker")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "Hi, I'm in healthy condition!";
+
+    let response_json = &GenericResponse {
+        status: "success".to_string(),
+        message: MESSAGE.to_string(),
+    };
+    HttpResponse.Ok().json(response_json)
+}
+
+
+#[get("/todos")]
+pub async fn todos_list_handler (
+    opts: web::Query<QueryOptions>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let todos = data.todo_db.lock().unwrap();
+
+    let limit = opts.limit.unwrap_or(10);
+    let offset = (opts.page.unwrap_or(1) - 1) * limit;
+    
+    let todos: Vec<Todo> = todos.clone().into_iter().skip(offset).take(limit).collect();
+    
+    let json_response = TodoListResponse {
+        status: "success".to_string(),
+        results: todos.len(),
+        todos,
+    };
+
+    HttpResponse.Ok().json(json_response);
+}
---
+++ tests/README.md
@@ -0,0 +1,6 @@
+---
+noteId: "2f88fa00353c11ee9bfd9dd1d382a28f"
+tags: []
+
+---
+
